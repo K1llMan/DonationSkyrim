32b9d65edea9b5cd8e7d3b4545ed9f58bbd5a581
K1llM@n <k1llmanws@yandex.ru>
2022-02-27 14:52:16 +0300

    Заготовка плагина для скайрима

---
+++ DonationSkyrim/DonationSkyrim/DonationSkyrimPlugin.cs
@@ -0,0 +1,16 @@
+﻿using NetScriptFramework;
+
+namespace DonationSkyrim
+{
+    public class DonationSkyrimPlugin : Plugin
+    {
+        public override string Key => "DonationSkyrimPlugin";
+        public override string Name => "Donation Skyrim Plugin";
+        public override int Version => 1;
+
+        protected override bool Initialize(bool loadedAny)
+        {
+            return base.Initialize(loadedAny);
+        }
+    }
+}
